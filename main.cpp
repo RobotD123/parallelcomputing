05d1bfc3fdb21ee62f6a13891f138e8414234897
hduan <hduan@uncc.edu>
2017-09-15 15:27:19 -0400

    Changes to be committed: 	new file:   approx 	deleted:    basicProgramming.tar 	new file:   ld 	new file:   main 	modified:   main.cpp

--- main.cpp
+++ main.cpp
@@ -24,23 +24,21 @@ int main (int argc, char* argv[]) {
   if (argc < 6) {
     std::cerr<<"usage: "<<argv[0]<<" <functionid> <a> <b> <n> <intensity>"<<std::endl;
     return -1;
-  }
-
-  return 0;
+  }  
 
   //type convert
   int n, functionid, intens; float a, b, sum, xp, temp;
-    functionid = atoi(argv[0]);
-    a = atof(argv[1]);
-    b = atof(argv[2]);
-    n = atoi(argv[3]);
-    intens = atoi(argv[4]);
+    functionid = atoi(argv[1]);
+    a = atof(argv[2]);
+    b = atof(argv[3]);
+    n = atoi(argv[4]);
+    intens = atoi(argv[5]);
 
 
   //  Call functions within an array
   switch(functionid) {
       case 1 :
-      for  (int i = 0; i< n-1 ; i++){
+      for  (int i = 0; i< n; i++){
 
           xp = a + (i+0.5)*(b-a)/n;
           temp += f1(xp,intens);
@@ -48,7 +46,7 @@ int main (int argc, char* argv[]) {
       }
          break;
       case 2 :
-      for  (int i = 0; i< n-1 ; i++){
+      for  (int i = 0; i< n; i++){
 
           xp = a + (i+0.5)*(b-a)/n;
           temp += f2(xp,intens);
@@ -57,7 +55,7 @@ int main (int argc, char* argv[]) {
       }
          break;
       case 3:
-      for  (int i = 0; i< n-1 ; i++){
+      for  (int i = 0; i< n; i++){
 
           xp = a + (i+0.5)*(b-a)/n;
           temp += f3(xp,intens);
@@ -66,7 +64,7 @@ int main (int argc, char* argv[]) {
       }
          break;
       case 4 :
-      for  (int i = 0; i< n-1 ; i++){
+      for  (int i = 0; i< n; i++){
 
           xp = a + (i+0.5)*(b-a)/n;
           temp += f4(xp,intens);
@@ -79,6 +77,7 @@ int main (int argc, char* argv[]) {
    }
 
 
-    cout << "The integral of function" << functionid << "from"<< a <<"to" << b << "is" << sum << endl;
+    cout << sum << endl;
 
 }
+
